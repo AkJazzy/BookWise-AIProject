613d07f33b410d06b62d6b20d7f4dcdcbdfbe010
AkJazzy <152028333+AkJazzy@users.noreply.github.com>
2025-04-30 21:45:52 +0530

    Delete app directory

--- app/retriever.py
+++
@@ -1,9 +0,0 @@
-from app.vector_store import query_vector_db
-from app.gemini_llm import generate_summary
-
-def answer_query(user_query):
-    results = query_vector_db(user_query, top_k=5)
-    docs = results["documents"][0]
-    combined_context = "\n\n".join(docs)
-    summary = generate_summary(combined_context, user_query)
-    return summary
